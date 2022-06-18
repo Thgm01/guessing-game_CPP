a80a859ac35b9b33ddc228c8ab868d2959f785a6
Thiago T. Moura <rpj134@gmail.com>
2022-06-18 16:55:50 -0300

    implementing score to the game

--- main.cpp
+++ main.cpp
@@ -13,6 +13,8 @@ int main()
     bool win = false;
     int attempts = 0;
 
+    float pontuation = 1000.0;
+
     do
     {
         attempts++;
@@ -21,6 +23,9 @@ int main()
         cout << "Attempt: " << attempts << endl << endl;
         cout << "What is your guess: ";
         cin >> guess;
+
+        float lost_points = abs(guess - SECRET_NUMBER)/2.0;
+        pontuation -= lost_points;
         
         bool greater = guess > SECRET_NUMBER;
         bool less = guess < SECRET_NUMBER;
@@ -36,9 +41,14 @@ int main()
         else
         {
             cout << "Congratulations! You got it rignt!\n";  
-            win = 1;      
+            win = 1; 
+                 
         }
     }while(!win);
     cout << "End game!" << endl;
-    cout << "You won in " << attempts << (attempts>1 ? " Attempts" : " Attempt") << endl;
+    cout << "You won in " << attempts << (attempts>1 ? " Attempts!" : " Attempt!") << endl;
+
+    cout.precision(2);
+    cout << fixed;
+    cout << "Your Score was: " << pontuation << (pontuation>1 ? " Points!" : " Point!") << endl; 
 }
